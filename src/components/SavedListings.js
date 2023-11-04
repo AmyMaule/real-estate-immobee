19fc38ddefa457151f24ef57daae391e48fe582d
Amy <amaule65@gmail.com>
2023-11-04 12:37:56 +0100

    Don't show hidden listings on saved listings page

--- src/components/SavedListings.js
+++ src/components/SavedListings.js
@@ -3,7 +3,12 @@ import React, { useState } from 'react';
 import ListingsContainer from './ListingsContainer';
 
 const SavedListings = () => {
-  const [listings, setListings] = useState(JSON.parse(localStorage.getItem("savedListings")) || []);
+  const getSavedListings = () => {
+    const allSavedListings = JSON.parse(localStorage.getItem("savedListings")) || [];
+    const hiddenListings = JSON.parse(localStorage.getItem("hiddenListings")) || [];
+    return allSavedListings.filter(listing => !hiddenListings.includes(listing.listingID));
+  }
+  const [listings, setListings] = useState(getSavedListings());
 
   return (
     <div className="saved-listings-page-container">
